3000a5879870dcbe2565ab8c3ea84f577f99834c
fk1000iq <fk1000iq@163.com>
2024-09-03 21:38:00 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace serial_port
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+			this.comboBoxPorts = new System.Windows.Forms.ComboBox();
+			this.label_serial_name = new System.Windows.Forms.Label();
+			this.richTextBox_log = new System.Windows.Forms.RichTextBox();
+			this.button_serial_onoff = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// comboBoxPorts
+			// 
+			this.comboBoxPorts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBoxPorts.DropDownWidth = 200;
+			this.comboBoxPorts.FormattingEnabled = true;
+			this.comboBoxPorts.Location = new System.Drawing.Point(58, 41);
+			this.comboBoxPorts.Name = "comboBoxPorts";
+			this.comboBoxPorts.Size = new System.Drawing.Size(121, 20);
+			this.comboBoxPorts.TabIndex = 0;
+			// 
+			// label_serial_name
+			// 
+			this.label_serial_name.AutoSize = true;
+			this.label_serial_name.Location = new System.Drawing.Point(13, 44);
+			this.label_serial_name.Name = "label_serial_name";
+			this.label_serial_name.Size = new System.Drawing.Size(29, 12);
+			this.label_serial_name.TabIndex = 1;
+			this.label_serial_name.Text = "串口";
+			// 
+			// richTextBox_log
+			// 
+			this.richTextBox_log.Location = new System.Drawing.Point(227, 41);
+			this.richTextBox_log.Name = "richTextBox_log";
+			this.richTextBox_log.Size = new System.Drawing.Size(467, 185);
+			this.richTextBox_log.TabIndex = 2;
+			this.richTextBox_log.Text = "";
+			// 
+			// button_serial_onoff
+			// 
+			this.button_serial_onoff.Location = new System.Drawing.Point(104, 203);
+			this.button_serial_onoff.Name = "button_serial_onoff";
+			this.button_serial_onoff.Size = new System.Drawing.Size(75, 23);
+			this.button_serial_onoff.TabIndex = 3;
+			this.button_serial_onoff.Text = "打开";
+			this.button_serial_onoff.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.button_serial_onoff);
+			this.Controls.Add(this.richTextBox_log);
+			this.Controls.Add(this.label_serial_name);
+			this.Controls.Add(this.comboBoxPorts);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBoxPorts;
+        private System.Windows.Forms.Label label_serial_name;
+        private System.Windows.Forms.RichTextBox richTextBox_log;
+        private System.Windows.Forms.Button button_serial_onoff;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Management;    //需要再引用中添加
+using Microsoft.Win32;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Threading;
+
+namespace serial_port
+{
+    public partial class Form1 : Form
+    {
+
+        private ManagementEventWatcher arrivalWatcher;
+        private ManagementEventWatcher removalWatcher;
+
+        public uint sum_val = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetupUsbEventWatchers();//热插拔动作设定
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UpdateSerialPortList();//上电初始化
+
+			//combobox 下拉显示宽度，下拉类型设置
+            comboBoxPorts.DropDownWidth = 200;
+			comboBoxPorts.DropDownStyle = ComboBoxStyle.DropDownList;//这样不能修改项
+		}
+
+
+        
+
+        private void DeviceArrival(object sender, EventArrivedEventArgs e)
+        {
+			DebounceUpdateSerialPortList();
+		}
+
+        private void DeviceRemoval(object sender, EventArrivedEventArgs e)
+        {
+			DebounceUpdateSerialPortList();
+		}
+
+		private System.Threading.Timer debounceTimer;
+		private void DebounceUpdateSerialPortList()
+		{
+			// 如果定时器已经存在，则取消它
+			if (debounceTimer != null)
+			{
+				debounceTimer.Change(Timeout.Infinite, Timeout.Infinite);
+				debounceTimer.Dispose();
+			}
+
+			// 设置一个新的定时器，在500毫秒后执行更新操作
+			debounceTimer = new System.Threading.Timer((state) =>
+			{
+				UpdateSerialPortList();
+			}, null, 500, Timeout.Infinite);
+		}
+
+
+		//public string[] portDevName = null;
+
+		/*
+            usb xxx xxx (COMx) ---> COMx #usb xxx xxx
+        */
+		private string ConvertStringOrder(string input)
+		{
+			// 找到左括号的位置
+			int startIndex = input.IndexOf('(');
+			// 找到右括号的位置
+			int endIndex = input.IndexOf(')');
+
+			// 提取括号内的内容（COM端口号）
+			string comPort = input.Substring(startIndex + 1, endIndex - startIndex - 1);
+			// 提取括号外的内容
+			string otherPart = input.Substring(0, startIndex) + input.Substring(endIndex + 1);
+
+			// 重新组合字符串
+			string result = $"{comPort} #{otherPart}";
+			return result;
+		}
+        
+        // 修改字符串组顺序
+		private string[] changeStringOrder(string[] strsIn)
+		{
+            string[] strsOut;
+			List<string> stringList = new List<string>();
+
+			foreach (string str in strsIn)
+			{
+				string str_new = ConvertStringOrder(str);
+				stringList.Add(str_new);
+			}
+			strsOut = stringList.ToArray();
+
+            return strsOut;
+		}
+		/*
+            1.需要添加名称空间
+            2.需要在“引用”中添加 System.Management
+         */
+		private string[] getPortDevName()
+		{
+            string[] portDevName;
+			using (ManagementObjectSearcher searcher = new ManagementObjectSearcher
+				("select * from Win32_PnPEntity where Name like '%(COM%'"))
+			{
+				List<string> stringList = new List<string>();
+
+				var hardInfos = searcher.Get();
+				foreach (var hardInfo in hardInfos)
+				{
+					if (hardInfo.Properties["Name"].Value != null)
+					{
+						string devName = hardInfo.Properties["Name"].Value.ToString(); //deviceName like--- "USB-Enhanced-SERIAL CH343 (COM8)"
+						Console.WriteLine(devName);
+						stringList.Add(devName);
+					}
+				}
+				portDevName = stringList.ToArray();
+			}
+
+			// portDevName as "USB-Enhanced-SERIAL CH343 (COM8)" --->portDevName_new as "COM8 #USB-Enhanced-SERIAL CH343" 
+			string[] portDevName_new = changeStringOrder(portDevName);
+  
+			return portDevName_new;
+		}
+
+
+		private void UpdateSerialPortList()
+        {
+            if (comboBoxPorts.InvokeRequired)
+            {
+                comboBoxPorts.Invoke(new MethodInvoker(UpdateSerialPortList));
+            }
+            else
+            {
+                comboBoxPorts.Items.Clear();//清除旧的下拉列表
+                string[] ports = getPortDevName();//获取当前可用串口信息
+				comboBoxPorts.Items.AddRange(ports);//更新下拉列表
+
+                if (comboBoxPorts.Items.Count > 0)//若存在至少一个串口
+                {
+                    comboBoxPorts.SelectedIndex = 0;    //choose the first one
+					comboBoxPorts.SelectionStart = 0;   //让光标回到最左侧，若是DropdownList，则不显示光标，显示内容从最左侧开始
+				}
+                else {//无串口则清除之前选中的串口的文本信息（注意清除列表时，不会清除Text, 需要自己额外清除）
+					comboBoxPorts.Text = string.Empty;  // clear Text
+				}
+            }
+        }
+
+
+        private void SetupUsbEventWatchers()
+        {
+            WqlEventQuery arrivalQuery = new WqlEventQuery("SELECT * FROM Win32_DeviceChangeEvent WHERE EventType = 2");
+            arrivalWatcher = new ManagementEventWatcher(arrivalQuery);
+            arrivalWatcher.EventArrived += new EventArrivedEventHandler(DeviceArrival);
+            arrivalWatcher.Start();
+
+            WqlEventQuery removalQuery = new WqlEventQuery("SELECT * FROM Win32_DeviceChangeEvent WHERE EventType = 3");
+            removalWatcher = new ManagementEventWatcher(removalQuery);
+            removalWatcher.EventArrived += new EventArrivedEventHandler(DeviceRemoval);
+            removalWatcher.Start();
+        }
+
+
+
+
+    }
+}
